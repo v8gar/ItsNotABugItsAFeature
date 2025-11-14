30f770f949f48cbcc47de2e9ecd796bb5a7b752d
Athish <Athish1123@gmail.com>
2025-11-13 18:41:38 -0600

    Fix

--- Enemy/enemy.gd
+++ Enemy/enemy.gd
@@ -2,7 +2,7 @@ class_name Enemy
 extends CharacterBody2D
 
 @onready var navigation_agent_2d: NavigationAgent2D = $NavigationAgent2D
-@export var movement_speed: float = 2.0
+@export var movement_speed: float = 1.0
 var new_velocity: Vector2
 
 @onready var player = get_tree().get_nodes_in_group("players")[0]
@@ -13,7 +13,6 @@ func _process(delta: float) -> void:
 	moveEnemy()
 	
 func moveEnemy():
-	print(player.global_position)
 	navigation_agent_2d.set_target_position(player.global_position)
 	velocity = global_position.direction_to(navigation_agent_2d.get_next_path_position()) * movement_speed
 	move_to_destination(velocity)
