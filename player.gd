181b41eacaed25089ff8b87b0f4f5b279262c6d7
Aria-Williams <159070680+Area9579@users.noreply.github.com>
2025-11-14 18:40:06 -0600

    Minor tweak

--- player.gd
+++ player.gd
@@ -25,7 +25,6 @@ func _process(delta: float) -> void:
 		if instanced_item == null:
 			instanced_item = item.instantiate().with_data(self)
 			add_child(instanced_item)
-			await instanced_item.ready
 		instanced_item.activate()
 	
 	move_and_slide()
