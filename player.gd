9c3989c3ebaa976765c2d154ec996b9e78e2363a
Aria-Williams <159070680+Area9579@users.noreply.github.com>
2025-11-16 15:34:12 -0600

    Working Inventory System

--- player.gd
+++ player.gd
@@ -4,10 +4,15 @@ const SPEED: int = 200
 var click_position = Vector2()
 var target_position = Vector2()
 
+@onready var inventory: Inventory = $CanvasLayer/Inventory
+
 @export var hitbox : HitBox
 @export var item : PackedScene
 
 var instanced_item : Item = null
+
+#func _ready() -> void:
+	#inventory.pickup_new_item(load("res://Items/item.tscn"))
 
 func _process(delta: float) -> void:
 	if Input.is_action_pressed("left_click"):
@@ -22,9 +27,9 @@ func _process(delta: float) -> void:
 	look_at(get_global_mouse_position())
 	
 	if Input.is_action_just_pressed("right_click"):
-		if instanced_item == null:
-			instanced_item = item.instantiate().with_data(self)
-			add_child(instanced_item)
-		instanced_item.activate()
+		inventory.activate()
+	
+	if Input.is_action_just_pressed("switch_item"):
+		inventory.swap_items()
 	
 	move_and_slide()
