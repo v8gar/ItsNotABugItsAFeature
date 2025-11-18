544e5b2edb853d859cff3264c06ea275bcb6826f
Eva Natalía <evaelvars@gmail.com>
2025-11-18 14:14:52 -0600

    Updated color changing method to be more vivid

--- Enemy/enemy.gd
+++ Enemy/enemy.gd
@@ -10,7 +10,6 @@ var direction : Vector2
 
 func _physics_process(delta: float) -> void:
 	move_enemy(delta, 100)
-	update_health()
 
 func move_enemy(delta : float, movement_speed: float) -> void:
 	navigation_agent_2d.target_position = player.global_position
@@ -30,21 +29,3 @@ func player_location() -> Vector2:
 	
 func die():
 	self.queue_free()
-	
-func update_health():
-	var healthbar = $HealthBar
-	var health = hit_box.health_component
-	healthbar.max_value = hit_box.health_component.max_health
-	healthbar.value = hit_box.health_component
-	if health >= hit_box.health_component.max_health:
-		healthbar.visible = false
-	else:
-		healthbar.visible = true
-	
-	var current_percentage = ((healthbar.value - healthbar.min_value) / (healthbar.max_value - healthbar.min_value)) * 100
-	if current_percentage >= 70:
-		healthbar.add_theme_color_override("fill_color", Color('#8ab060'))
-	elif current_percentage >= 30:
-		healthbar.add_theme_color_override("fill_color", Color('#d3a068'))
-	else:
-		healthbar.add_theme_color_override("fill_color", Color('#b45252'))
